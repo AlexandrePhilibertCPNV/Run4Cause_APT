9abdbbdb1e232d639c288df2bcefcfe8b25a38ae
Alexandre Philibert <alexandre.philibert@cpnv.ch>
2022-03-03 09:24:39 +0100

    Setup database service

--- Program.cs
+++ Program.cs
@@ -1,7 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Run4Cause.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<Run4CauseContext>(options =>
+{
+    options
+        .UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+        .UseSnakeCaseNamingConvention();
+});
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 var app = builder.Build();
 
@@ -11,6 +22,14 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+
+// Check if the database is created. If not, create it.
+using (IServiceScope scope = app.Services.CreateScope())
+{
+    IServiceProvider services = scope.ServiceProvider;
+    Run4CauseContext context = services.GetRequiredService<Run4CauseContext>();
+    context.Database.EnsureCreated();
 }
 
 app.UseHttpsRedirection();
