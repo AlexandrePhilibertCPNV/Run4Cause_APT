df9bc9ef53d7b85c16af61e7b8f7be4f4c25f376
Alexandre Philibert <alexandre.philibert@cpnv.ch>
2022-03-31 10:47:00 +0200

    Create migration for TotalDistanceCovered

---
+++ Migrations/20220331084341_ChangeTotalDistanceCoveredType.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Run4Cause.Migrations
+{
+    public partial class ChangeTotalDistanceCoveredType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_profile_users_user_id",
+                table: "profile");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "pk_profile",
+                table: "profile");
+
+            migrationBuilder.RenameTable(
+                name: "profile",
+                newName: "profiles");
+
+            migrationBuilder.RenameIndex(
+                name: "ix_profile_user_id",
+                table: "profiles",
+                newName: "ix_profiles_user_id");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "total_distance_covered",
+                table: "users",
+                type: "numeric(20,2)",
+                nullable: true,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "email",
+                table: "users",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<int>(
+                name: "access_failed_count",
+                table: "users",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "concurrency_stamp",
+                table: "users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "email_confirmed",
+                table: "users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "lockout_enabled",
+                table: "users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "lockout_end",
+                table: "users",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "normalized_email",
+                table: "users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "normalized_user_name",
+                table: "users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "password_hash",
+                table: "users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "phone_number_confirmed",
+                table: "users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "security_stamp",
+                table: "users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "two_factor_enabled",
+                table: "users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "user_name",
+                table: "users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "pk_profiles",
+                table: "profiles",
+                column: "id");
+
+            migrationBuilder.CreateTable(
+                name: "user_claims",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    user_id = table.Column<int>(type: "integer", nullable: false),
+                    claim_type = table.Column<string>(type: "text", nullable: true),
+                    claim_value = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_user_claims", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_logins",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    login_provider = table.Column<string>(type: "text", nullable: true),
+                    provider_key = table.Column<string>(type: "text", nullable: true),
+                    provider_display_name = table.Column<string>(type: "text", nullable: true),
+                    user_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_user_logins", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_roles",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    user_id = table.Column<int>(type: "integer", nullable: false),
+                    role_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_user_roles", x => x.id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "users",
+                columns: new[] { "id", "access_failed_count", "concurrency_stamp", "email", "email_confirmed", "first_name", "last_name", "lockout_enabled", "lockout_end", "normalized_email", "normalized_user_name", "password_hash", "phone_number", "phone_number_confirmed", "profile_id", "security_stamp", "total_distance_covered", "two_factor_enabled", "user_name" },
+                values: new object[] { 1, 0, "7b5d7e33-2f17-490c-b4e6-44d9c4ebb912", "alexandre.philibert@cpnv.ch", false, "Alexandre", "Philibert", false, null, null, null, null, "0781234567", false, null, null, 12.4m, false, null });
+
+            migrationBuilder.InsertData(
+                table: "profiles",
+                columns: new[] { "id", "description", "image", "photos", "user_id" },
+                values: new object[] { 1, "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores, vero!", null, null, 1 });
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_profiles_users_user_id",
+                table: "profiles",
+                column: "user_id",
+                principalTable: "users",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_profiles_users_user_id",
+                table: "profiles");
+
+            migrationBuilder.DropTable(
+                name: "user_claims");
+
+            migrationBuilder.DropTable(
+                name: "user_logins");
+
+            migrationBuilder.DropTable(
+                name: "user_roles");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "pk_profiles",
+                table: "profiles");
+
+            migrationBuilder.DeleteData(
+                table: "profiles",
+                keyColumn: "id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "users",
+                keyColumn: "id",
+                keyValue: 1);
+
+            migrationBuilder.DropColumn(
+                name: "access_failed_count",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "concurrency_stamp",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "email_confirmed",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "lockout_enabled",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "lockout_end",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "normalized_email",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "normalized_user_name",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "password_hash",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "phone_number_confirmed",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "security_stamp",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "two_factor_enabled",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "user_name",
+                table: "users");
+
+            migrationBuilder.RenameTable(
+                name: "profiles",
+                newName: "profile");
+
+            migrationBuilder.RenameIndex(
+                name: "ix_profiles_user_id",
+                table: "profile",
+                newName: "ix_profile_user_id");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "total_distance_covered",
+                table: "users",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(20,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "email",
+                table: "users",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "pk_profile",
+                table: "profile",
+                column: "id");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_profile_users_user_id",
+                table: "profile",
+                column: "user_id",
+                principalTable: "users",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
