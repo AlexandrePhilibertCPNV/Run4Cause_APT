164bbade0eb7bdf604e0b8f975a6211eba3e25ed
Alexandre Philibert <alexandre.philibert@cpnv.ch>
2022-03-31 15:52:54 +0200

    Run tailwind in watch mode in dev environment

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using Run4Cause.Data;
 using Run4Cause.Models;
 using Microsoft.AspNetCore.Identity;
+using Tailwind;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -30,6 +31,10 @@ if (!app.Environment.IsDevelopment())
     // app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    app.RunTailwind("css:watch", "./");
 }
 
 // Check if the database is created. If not, create it.
